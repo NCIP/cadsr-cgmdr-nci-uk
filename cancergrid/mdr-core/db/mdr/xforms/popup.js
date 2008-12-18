31c6594af0adf89ba93b32e61033294796975977
pwtsui <pwtsui>
2008-12-18 13:51:18 +0000

    Fixed element not found error.
    SVN-Revision: 6562

--- cancergrid/mdr-core/db/mdr/xforms/popup.js
+++ cancergrid/mdr-core/db/mdr/xforms/popup.js
@@ -11,7 +11,8 @@ function updateOpener(tag, value)
    var myDocument = window.opener.document;
    var myModel = myDocument.getElementById("myModel");
    var myInstance = myModel.getInstanceDocument("main");
-   var myNode = myInstance.getElementsByTagName(tag.substring(tag.indexOf(':') + 1));
+   //var myNode = myInstance.getElementsByTagName(tag.substring(tag.indexOf(':') + 1));
+   var myNode = myInstance.getElementsByTagName(tag);
    
    myNode[myNode.length - 1].textContent = value;   
    void fullModelRebuild(myModel);
